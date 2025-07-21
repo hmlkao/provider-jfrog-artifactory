cd5758548fd9af4351a3bdcee295b7ab2532bc1b
Ondrej Homolka <ondrej.homolka@merck.com>
2025-07-21 14:11:04 +0200

    Add generated files for docs fix

--- apis/artifactory/v1alpha1/zz_anonymoususer_types.go
+++ apis/artifactory/v1alpha1/zz_anonymoususer_types.go
@@ -52,7 +52,7 @@ type AnonymousUserStatus struct {
 // +kubebuilder:subresource:status
 // +kubebuilder:storageversion
 
-// AnonymousUser is the Schema for the AnonymousUsers API.
+// AnonymousUser is the Schema for the AnonymousUsers API. ""
 // +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
 // +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="EXTERNAL-NAME",type="string",JSONPath=".metadata.annotations.crossplane\\.io/external-name"
