37777d4f391ab7f4ddf55f797d9805d5e5a87f97
Ondrej Homolka <ondrej.homolka@merck.com>
2025-09-03 11:39:03 +0200

    Fix extensions append to XPKG package

--- custom.mk
+++ custom.mk
@@ -3,7 +3,7 @@
 define xpkg.set-extensions
 xpkg.set-extensions.$(1).$(2): $(UP)
 	@$(INFO) Setting extensions for package $(1)/$(2):$(VERSION)
-	@$(UP) alpha xpkg append \
+	@$(UP) xpkg append \
 		--extensions-root=./extensions \
 		$(1)/$(2):$(VERSION)
 	@$(OK) Extensions set for package $(1)/$(2):$(VERSION)
@@ -11,8 +11,8 @@ xpkg.set-extensions: xpkg.set-extensions.$(1).$(2)
 endef
 $(foreach r,$(XPKG_REG_ORGS), $(foreach x,$(XPKGS), $(eval $(call xpkg.set-extensions,$(r),$(x)))))
 
-# Only set extensions for specific branches
+# Set extensions only for specific branches
 set-extensions: ; @:
-ifneq ($(filter main master release-%,$(BRANCH_NAME)),)
+ifneq ($(filter $(RELEASE_BRANCH_FILTER),$(BRANCH_NAME)),)
 set-extensions: $(foreach r,$(XPKG_REG_ORGS), $(foreach x,$(XPKGS),xpkg.set-extensions.$(r).$(x)))
 endif
