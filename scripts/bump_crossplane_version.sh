13c0566d1e569ace526fbecec8c84a43eb8fa8d7
Ondrej Homolka <ondrej.homolka@gmail.com>
2026-09-20 20:03:07 +0200

    Add script to bump Go lang version

--- scripts/bump_crossplane_version.sh
+++ scripts/bump_crossplane_version.sh
@@ -7,19 +7,20 @@ IFS=$'\n\t'
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 current_version=${1}
-if [[ -z "$current_version" ]]; then
+if [[ -z "${current_version}" ]]; then
   echo "::error Current version is empty"
   exit 1
 fi
 new_version=${2}
-if [[ -z "$new_version" ]]; then
+if [[ -z "${new_version}" ]]; then
   echo "::error New version is empty"
   exit 1
 fi
 
 main() {
-  sed -i'.tmp' "s|CROSSPLANE_VERSION = ${current_version}|CROSSPLANE_VERSION = ${new_version}|" "$dir/../Makefile" && rm "$dir/../Makefile.tmp"
-  sed -i'.tmp' "s|CROSSPLANE_CLI_VERSION = v${current_version}|CROSSPLANE_CLI_VERSION = v${new_version}|" "$dir/../Makefile" && rm "$dir/../Makefile.tmp"
+  # Use of sed -i'.tmp' is to make it compatible with both GNU and BSD sed. BSD sed requires an argument for the -i option, so we provide a temporary file extension '.tmp' to create a backup of the original file before making in-place edits. After the edits are made, we remove the temporary backup file.
+  sed -i'.tmp' "s|CROSSPLANE_VERSION = ${current_version}|CROSSPLANE_VERSION = ${new_version}|" "${dir}/../Makefile" && rm "${dir}/../Makefile.tmp"
+  sed -i'.tmp' "s|CROSSPLANE_CLI_VERSION = v${current_version}|CROSSPLANE_CLI_VERSION = v${new_version}|" "${dir}/../Makefile" && rm "${dir}/../Makefile.tmp"
 }
 
 main "$@"
