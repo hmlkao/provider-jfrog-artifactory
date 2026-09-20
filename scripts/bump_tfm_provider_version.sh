ca0449f6414fa8d37e211c034bee6f0e5068e12f
Ondrej Homolka <ondrej.homolka@gmail.com>
2026-09-20 20:03:07 +0200

    Add script to bump Go lang version

--- scripts/bump_tfm_provider_version.sh
+++ scripts/bump_tfm_provider_version.sh
@@ -7,22 +7,23 @@ IFS=$'\n\t'
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 current_version=${1}
-if [[ -z "$current_version" ]]; then
+if [[ -z "${current_version}" ]]; then
   echo "::error Current version is empty"
   exit 1
 fi
 new_version=${2}
-if [[ -z "$new_version" ]]; then
+if [[ -z "${new_version}" ]]; then
   echo "::error New version is empty"
   exit 1
 fi
 
 main() {
-  sed -i'.tmp' "s|export TERRAFORM_PROVIDER_VERSION ?= ${current_version}|export TERRAFORM_PROVIDER_VERSION ?= ${new_version}|g" "$dir/../Makefile" && rm "$dir/../Makefile.tmp"
-  sed -i'.tmp' "s|terraform-provider-artifactory_v${current_version}|terraform-provider-artifactory_v${new_version}|g" "$dir/../Makefile" && rm "$dir/../Makefile.tmp"
-  sed -i'.tmp' "s|jfrog/artifactory v${current_version}|jfrog/artifactory v${new_version}|g" "$dir/../README.md" && rm "$dir/../README.md.tmp"
-  sed -i'.tmp' "s|Terraform provider v${current_version}|Terraform provider v${new_version}|g" "$dir/../README.md" && rm "$dir/../README.md.tmp"
-  sed -i'.tmp' "s|jfrog/artifactory/${current_version}|jfrog/artifactory/${new_version}|g" "$dir/../README.md" && rm "$dir/../README.md.tmp"
+  # Use of sed -i'.tmp' is to make it compatible with both GNU and BSD sed. BSD sed requires an argument for the -i option, so we provide a temporary file extension '.tmp' to create a backup of the original file before making in-place edits. After the edits are made, we remove the temporary backup file.
+  sed -i'.tmp' "s|export TERRAFORM_PROVIDER_VERSION ?= ${current_version}|export TERRAFORM_PROVIDER_VERSION ?= ${new_version}|g" "${dir}/../Makefile" && rm "${dir}/../Makefile.tmp"
+  sed -i'.tmp' "s|terraform-provider-artifactory_v${current_version}|terraform-provider-artifactory_v${new_version}|g" "${dir}/../Makefile" && rm "${dir}/../Makefile.tmp"
+  sed -i'.tmp' "s|jfrog/artifactory v${current_version}|jfrog/artifactory v${new_version}|g" "${dir}/../README.md" && rm "${dir}/../README.md.tmp"
+  sed -i'.tmp' "s|Terraform provider v${current_version}|Terraform provider v${new_version}|g" "${dir}/../README.md" && rm "${dir}/../README.md.tmp"
+  sed -i'.tmp' "s|jfrog/artifactory/${current_version}|jfrog/artifactory/${new_version}|g" "${dir}/../README.md" && rm "${dir}/../README.md.tmp"
 }
 
 main "$@"
